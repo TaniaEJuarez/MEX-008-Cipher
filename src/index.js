73cb1ffbea9f7aec15ce1fe055d09c4730af210d
TaniaEJuarez <telizabethjdiaz@gmail.com>
2019-06-13 19:21:25 -0500

    Subiendo estructura de proyecto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.getElementById("cifrar").addEventListener("click", ()=>{
+  location.href = "cifrar.html"
+})
+
+document.getElementById("descifrar").addEventListener("click", ()=>{
+  location.href = "descifrar.html"
+})
