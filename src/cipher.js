d73a1ccf25acb6d5676b25bec6378dd6b97036a9
TaniaEJuarez <telizabethjdiaz@gmail.com>
2019-06-18 20:12:52 -0500

    Pasando el test de minusculas y caracteres especiales

--- src/cipher.js
+++ src/cipher.js
@@ -3,40 +3,48 @@ window.cipher = {
     //convertir el string en number
     offset=parseInt(offset);
     //convertir el string en letras mayusculas
-    let strToEncodeUp = strToEncode.toUpperCase();
+    let strToEncodeUp = strToEncode;
     //guardar el dato en una string vacia
     let strEncode="";
     //aplicar el ciclo for
     for (let i = 0; i < strToEncodeUp.length; i++) {
     //convertir el string en mayusculas
-      let charToEncodeASCII = strToEncodeUp.charCodeAt(i);
-    // aplicar la formula de cifrado cesar (x-65+n)%26+65
+    let charToEncodeASCII = strToEncodeUp.charCodeAt(i);
+
+    if(charToEncodeASCII >= 65 && charToEncodeASCII <= 90 ){
       let valorEncode = (charToEncodeASCII- 65 +offset)%26 +65;
       strEncode = strEncode + String.fromCharCode(valorEncode);
+    } else if(charToEncodeASCII >= 97 && charToEncodeASCII <= 122){
+      let valorEncode = (charToEncodeASCII- 97 +offset)%26 +97;
+      strEncode = strEncode + String.fromCharCode(valorEncode);
+    } else {
+      strEncode = strEncode + String.fromCharCode(charToEncodeASCII)
+    }
+    // aplicar la formula de cifrado cesar (x-65+n)%26+65
     }
     return strEncode;
   },
 
 // retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33
-// condicionar si ingresa minusculas, regresar minusculas
+// condicionar si ingresa minusculas, regresar minusculas <
 
-  decode : () => {
-  let strToDecode=  document.getElementById('descifrarContraseña').value;
-  let offset = document.getElementById('offsetDescifrado').value;
-  offset=parseInt(offset);
-  let cajaResultado = document.getElementById('contraseñaDescifrada');
-    //guardar el dato en una string vacia
-    let strDecode="";
-    //aplicar el ciclo for
-    for (let i = 0; i < strToDecode.length; i++) {
-    // aplicar la formula de cifrado cesar
-    // (x+n)%26
-      let valorDecode = (i+offset)%26;
-      let charToDecodeASCII = strToDecode.charCodeAt(i)-valorDecode;
-      strDecode = strDecode + String.fromCharCode(charToDecodeASCII);
-  }
-  cajaResultado.value = strDecode;
-}
+//   decode : () => {
+//   let strToDecode=  document.getElementById('descifrarContraseña').value;
+//   let offset = document.getElementById('offsetDescifrado').value;
+//   offset=parseInt(offset);
+//   let cajaResultado = document.getElementById('contraseñaDescifrada');
+//     //guardar el dato en una string vacia
+//     let strDecode="";
+//     //aplicar el ciclo for
+//     for (let i = 0; i < strToDecode.length; i++) {
+//     // aplicar la formula de cifrado cesar
+//     // (x+n)%26
+//       let valorDecode = (i+offset)%26;
+//       let charToDecodeASCII = strToDecode.charCodeAt(i)-valorDecode;
+//       strDecode = strDecode + String.fromCharCode(charToDecodeASCII);
+//   }
+//   cajaResultado.value = strDecode;
+// }
 };
 
 
